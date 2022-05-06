296fdd6bb79536b581197569c52569453fa81b29
Manoel Luis <manoelluisbusiness@gmail.com>
2022-05-06 13:24:31 -0300

    Niveis de dificuldade adicionados

--- app-mosquito/jogo.js
+++ app-mosquito/jogo.js
@@ -2,6 +2,21 @@ var altura = 0
 var largura = 0
 var vidas = 1
 var tempo = 5
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace ('?', '')
+
+if (nivel === 'normal') {
+	criaMosquitoTempo = 1500
+
+} else if (nivel === 'dificil') {
+	criaMosquitoTempo = 1000
+
+} else if (nivel === 'goku'){
+	criaMosquitoTempo = 750
+}
+
 
 function mudatamanho () {
 	altura = window.innerHeight
