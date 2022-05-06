743c328f83ce3b56037aafdc558961d0185ecc14
Manoel Luis <manoelluisbusiness@gmail.com>
2022-05-06 08:52:54 -0300

    Cronometro adicionado

--- app-mosquito/jogo.js
+++ app-mosquito/jogo.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 5
 
 function mudatamanho () {
 	altura = window.innerHeight
@@ -8,8 +9,24 @@ function mudatamanho () {
 
 	console.log(altura, largura)
 }
-
 mudatamanho()
+
+
+
+var cronometro = setInterval(function() {
+	
+	tempo -=1
+	if(tempo < 0) {
+
+		clearInterval(cronometro)
+		clearInterval(criamosca)
+		
+		alert('vitoria')
+	} else {
+
+	document.getElementById('cronometro').innerHTML = tempo
+	}
+} ,1000)
 
 	
 function posicaoRandomica () {
